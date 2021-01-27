aa1b0e4a3a546b48761d418b7403355bb40baa70
Damian Deza <damiandeza@gmail.com>
2021-01-26 23:36:15 -0300

    Add project files.

---
+++ CockaIO/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace CockaIO.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
