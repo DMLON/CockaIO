4d0b2bf1333514de5d1f07819e17bb32b6b43474
Damian Deza <damiandeza@gmail.com>
2021-01-29 18:47:53 -0300

    Added EF core

--- CockaIO/ViewModels/MainWindowViewModel.cs
+++ CockaIO/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,18 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using ReactiveUI;
 
 namespace CockaIO.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        ViewModelBase content;
+        public ViewModelBase Content
+        {
+            get => content;
+            private set => this.RaiseAndSetIfChanged(ref content, value);
+        }
         public string Greeting => "Welcome to Avalonia!";
     }
 }
